4f9f2603ce4b883f7509db1dd70a6923d2c664fb
Asilbek217 <yoldashaliyev0303@gmail.com>
2023-12-05 11:19:48 +0500

    ///
    //////

---
+++ code.js
@@ -0,0 +1,20 @@
+"Use strict"
+
+function catAndMouse(x, y, z) {
+
+    let catA = Math.abs(x-z)
+    let catB = Math.abs(y-z)
+
+    if (catA > catB) {
+        return "Cat A"
+    }
+    else if (catB > catA) {
+        return "Cat B"
+    }
+    else {
+        return "Mouse C"
+    }
+
+}
+
+console.log(catAndMouse(1,2,3));
